ccba1191ff17b2e94ce933d6e1bc3efbc7cdcdf9
Guilherme Catelli Bichaco <guicomercial328@gmail.com>
2025-03-06 09:14:55 -0300

    Implementando enunciado no EX 2

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,9 @@ console.log(meuCarro)
 
 //Exercício 2
 
+/*Crie uma interface Multiplicacao para tipar uma função que recebe dois números e retorna o resultado da multiplicação deles.
+Depois, implemente essa função e teste com diferentes valores.*/
+
 interface Multiplicacao {
     (a: number, b: number): number;
 }
